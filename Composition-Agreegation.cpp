0967b3d04ec0090aff7ca9ed133e688d5e9dc901
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-01-06 02:12:09 +0530

    Create Composition-Agreegation.cpp

---
+++ Composition-Agreegation.cpp
@@ -0,0 +1,22 @@
+Composition is even stronger relationship than aggregation. 
+To test if we are dealing with composition, use no sharing rule for composition:
+part can belong to only one whole.
+
+Besides the no sharing rule, another the rule for composition is: if the whole is destroyed,
+is the part that makes it up also destroyed?
+
+Code Example: A university owns various departments, and each department has a number of professors. 
+If the university closes, the departments will no longer exist, but the professors in those departments
+will continue to exist. Therefore, a University can be seen as a composition of departments, 
+whereas departments have an aggregation of professors. 
+In addition, a Professor could work in more than one department, but a department could not be part
+of more than one university.
+
+University - Department : composition: ownership: destroyed with the whole: strong has a relationship 
+
+The composite object takes ownership of the component. This means the composite is responsible 
+for the creation and destruction of the component parts. An object may only be part of one composite. 
+If the composite object is destroyed, all the component parts must be destroyed.
+Composition enforces encapsulation as the component parts usually are members of the composite object:
+
+Department - Professors: aggregation: no ownership: may outlive: component may still have weak has a relationship 
