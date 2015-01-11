6814fa2e57d2759c90d1b71edc53af4b93b9cea1
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-01-12 02:13:36 +0530

    Update AbstractFactory.cpp

--- AbstractFactory.cpp
+++ AbstractFactory.cpp
@@ -94,3 +94,135 @@ int main()
 
 	return 0;
 }
+
+
+Now if we add iPhone factory also we make minimal change in code
+#include <iostream>
+
+class Button
+{
+public:
+	virtual void paint() = 0;
+};
+ 
+class WinButton : public Button 
+{
+public:
+	void paint (){
+		std::cout << " Window Button \n";
+       }
+};
+ 
+class MacButton : public Button 
+{
+public:
+	void paint (){
+		std::cout << " Mac Button \n";
+       }
+};
+
+class iPhoneButton : public Button 
+{
+public:
+	void paint (){
+		std::cout << " iPhone Button \n";
+       }
+};
+
+class ScrollBar 
+{
+public:
+	virtual void paint() = 0;
+};
+ 
+class WinScrollBar : public ScrollBar 
+{
+public:
+	void paint (){
+		std::cout << " Window ScrollBar \n";
+       }
+};
+ 
+class MacScrollBar : public ScrollBar 
+{
+public:
+	void paint (){
+		std::cout << " Mac ScrollBar \n";
+       }
+};
+
+class iPhoneScrollBar : public ScrollBar
+{
+public:
+	void paint (){
+		std::cout << " iPhone ScrollBar \n";
+       }
+};
+
+
+class GUIFactory 
+{
+public:
+	virtual Button* createButton () = 0;
+	virtual ScrollBar* createScrollBar () = 0;
+};
+ 
+class WinFactory : public GUIFactory 
+{
+public:
+	Button* createButton (){
+		return new WinButton;
+	}
+	ScrollBar* createScrollBar (){
+		return new WinScrollBar;
+	}
+};
+ 
+class MacFactory : public GUIFactory 
+{
+public:
+	Button* createButton (){
+		return new MacButton;
+	}
+	ScrollBar* createScrollBar (){
+		return new MacScrollBar;
+	}
+};
+ 
+class iPhoneFactory : public GUIFactory 
+{
+public:
+	Button* createButton (){
+		return new iPhoneButton;
+	}
+	ScrollBar* createScrollBar (){
+		return new iPhoneScrollBar;
+	}
+};
+
+int main()
+{
+	GUIFactory* guiFactory;
+	Button *btn;
+	ScrollBar *sb;
+
+	guiFactory = new MacFactory;
+	btn = guiFactory->createButton();
+	btn -> paint();
+	sb = guiFactory->createScrollBar();
+	sb -> paint();
+
+	guiFactory = new WinFactory;
+	btn = guiFactory->createButton();
+	btn -> paint();
+	sb = guiFactory->createScrollBar();
+	sb -> paint();
+
+	guiFactory = new iPhoneFactory;
+	btn = guiFactory->createButton();
+	btn -> paint();
+	sb = guiFactory->createScrollBar();
+	sb -> paint();
+
+	return 0;
+}
