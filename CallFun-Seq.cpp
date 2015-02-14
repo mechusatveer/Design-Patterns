204429acfbcab376576d7744c1f3791a335c78eb
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-02-14 11:16:45 +0530

    Update CallFun-Seq.cpp

--- CallFun-Seq.cpp
+++ CallFun-Seq.cpp
@@ -12,12 +12,14 @@ how we can make it so always they get called in seq
 lock1.lock();
 lock2.lock();
 
+
 tid0 = pthread_t(&tid,NULL,&fun1,NULL);
 tid2 = pthread_t(&tid,NULL,&fun2,NULL);
 tid3 = pthread_t(&tid,NULL,&fun3,NULL);
 
 fun1()
 {
+   lock3.lock():
    do your work first;
    lock1.unlock();
 }
@@ -33,6 +35,7 @@ fun3()
 {
    lock2.lock();
    do your job here
+   lock3.unlock();
 }
 
 only two mutex are sufficient here
