bc96d97e99510fa54fb8d692def276a0a3cfe5be
Satveer Singh Mechu <mechu.satveer@gmail.com>
2015-02-14 11:11:39 +0530

    Create CallFun-Seq.cpp

---
+++ CallFun-Seq.cpp
@@ -0,0 +1,38 @@
+
+
+There are 3 threads each calling 
+
+fun1
+fun2
+fun3
+
+how we can make it so always they get called in seq
+
+
+lock1.lock();
+lock2.lock();
+
+tid0 = pthread_t(&tid,NULL,&fun1,NULL);
+tid2 = pthread_t(&tid,NULL,&fun2,NULL);
+tid3 = pthread_t(&tid,NULL,&fun3,NULL);
+
+fun1()
+{
+   do your work first;
+   lock1.unlock();
+}
+
+fun2()
+{
+    lock1.lock();
+    do your job here
+    lock2.unlokc();
+}
+
+fun3()
+{
+   lock2.lock();
+   do your job here
+}
+
+only two mutex are sufficient here
